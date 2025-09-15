9577e9cd58c298beb69385dbef5a550bad1cb66c
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-09-15 11:44:24 +0300

    set up input no ai version

--- hash_own.cpp
+++ hash_own.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main() {
 
-    cout << "Hello, world!";
+    int ivesties_tipas;
+    string ivestis, ivesties_failas;
+
+    cout << "Kaip norėsite įvesti tekstą? [0 - rankinis įvedimas, 1 - įvedimas iš failo]" << endl;
+    cin >> ivesties_tipas;
+
+    if (ivesties_tipas == 0) {
+        cout << "Iveskite hash'uojamą string'ą: ";
+        cin >> ivestis;
+    }
+
+    else if (ivesties_tipas == 1) {
+        cout << "Iveskite failo pavadinimą formatu [pavadinimas.txt]: ";
+        cin >> ivesties_failas;
+        
+        ifstream input(ivesties_failas);
+        input >> ivestis;
+    }  
+
+    cout << ivestis;
+
+
+
+
 }
