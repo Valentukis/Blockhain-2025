8371e82fd3ff9e5af1fe77f2bde04203221bc727
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-09-19 20:50:21 +0300

    Sukurti rotacija pradiniu byte'u

--- hash_own.cpp
+++ hash_own.cpp
@@ -2,13 +2,17 @@
 #include <fstream>
 #include <bitset>
 #include <vector>
+#include <cstdint>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
 
     int ivesties_tipas;
     string ivestis, ivesties_failas, ivestis_bin;
-    vector <string> seperate_bytes;
+    vector <bitset<8>> seperate_bytes;
+    uint64_t seed = 371928463890165017; 
 
     cout << "Kaip norėsite įvesti tekstą? [0 - rankinis įvedimas, 1 - įvedimas iš failo]" << endl;
     cin >> ivesties_tipas;
@@ -30,9 +34,16 @@ int main() {
     for (char c: ivestis) {
         bitset<8> bits(c);
         
-        seperate_bytes.push_back(bits.to_string());
+        seperate_bytes.push_back(bits);
 
     }
+
+    for (auto n: seperate_bytes) {
+        cout << n << " ";
+    }
+    cout << endl;
+    int stumimas = (ivestis.size() * 4683726410483) % seperate_bytes[0].count();
+    rotate(seperate_bytes.begin(), seperate_bytes.begin() + stumimas, seperate_bytes.end());
 
     for (auto n: seperate_bytes) {
         cout << n << " ";
