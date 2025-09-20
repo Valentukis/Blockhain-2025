b839015fa027252caa706dc818157c91c8b0a617
Valentinas <181562313+Valentukis@users.noreply.github.com>
2025-09-20 14:50:08 +0300

    Uzbaigti nuosava hash'a

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 Pradinė nuosavo hash'o idėja:
 
-1. Pasirinksime kokį nors pradinį hashą
-2. Atskirsime inputą į bitus
+1. Pasirinksime kokį nors pradinį seed'a
+2. Atskirsime inputą į byte'us
 3. Pagal tam tikrą tvarką išmaišysime juos
 4. Einant iš eilės, lyginsime bitus iš pradinio hash'o ir bitus iš sumaišyto input'o (Konjunkcija arba griežtoji disjunkcija, reikės patestuoti)
-5. Tą bitą iš hash'o pakeisime naujuoju gautu iš loginės operacijos
+5. Tą bitą iš seed'o pakeisime naujuoju gautu iš loginės operacijos
 (Optional) 6. Dar kartelį išmaiyšti gautą hash'ą
 
--- hash_own.cpp
+++ hash_own.cpp
@@ -4,53 +4,61 @@
 #include <vector>
 #include <cstdint>
 #include <algorithm>
-
+#include <limits>
+#include <iomanip>
 using namespace std;
-
+// 0x055D3F9EC866DB6C
+// 0x054C33BBC571D86D
+// 0x604016D6C571D86D
 int main() {
-
     int ivesties_tipas;
-    string ivestis, ivesties_failas, ivestis_bin;
-    vector <bitset<8>> seperate_bytes;
-    uint64_t seed = 371928463890165017; 
+    string ivestis, ivesties_failas;
+    vector<bitset<8>> separate_bytes;
+    uint64_t seed = 371928463890165017ull; 
 
     cout << "Kaip norėsite įvesti tekstą? [0 - rankinis įvedimas, 1 - įvedimas iš failo]" << endl;
     cin >> ivesties_tipas;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 
     if (ivesties_tipas == 0) {
-        cout << "Iveskite hash'uojamą string'ą: ";
-        cin >> ivestis;
+        cout << "Iveskite hash'uojamą string'ą: " << endl;
+        getline(cin, ivestis);
     }
 
     else if (ivesties_tipas == 1) {
-        cout << "Iveskite failo pavadinimą formatu [pavadinimas.txt]: ";
-        cin >> ivesties_failas;
-        
+        cout << "Iveskite failo pavadinimą formatu [pavadinimas.txt]: " << endl;
+        getline(cin, ivesties_failas);
         ifstream input(ivesties_failas);
-        input >> ivestis;
-    }  
-
-
-    for (char c: ivestis) {
-        bitset<8> bits(c);
-        
-        seperate_bytes.push_back(bits);
-
+        getline(input, ivestis);
+    }
+     
+    else {
+        cout << "Neteisingas pasirinkimas.\n";
+        return 0;
     }
 
-    for (auto n: seperate_bytes) {
-        cout << n << " ";
+    for (unsigned char c : ivestis) { 
+        separate_bytes.push_back(bitset<8>(c));
     }
-    cout << endl;
-    int stumimas = (ivestis.size() * 4683726410483) % seperate_bytes[0].count();
-    rotate(seperate_bytes.begin(), seperate_bytes.begin() + stumimas, seperate_bytes.end());
 
-    for (auto n: seperate_bytes) {
-        cout << n << " ";
+    //Permaisyt byetus
+    if (!separate_bytes.empty()) {
+        int k = (ivestis.size() * 781928401873 + separate_bytes.front().count()) % separate_bytes.size();
+        if (k == 0 && separate_bytes.size() > 1) k = 1;     // Butinas persukimas
+        rotate(separate_bytes.begin(), separate_bytes.begin() + k, separate_bytes.end());
     }
+
+    std::bitset<64> hash(seed); //xor kiekviena bit'a is seed ir input
+    int index = 0;
+    for (const auto& byte : separate_bytes) {
+
+        for (int b = 0; b < 8; ++b, ++index) {
+        int pos = index % 64; 
+        hash[pos] = hash[pos] ^ byte[b];
+        }
+    }
+
+    uint64_t h = hash.to_ullong();
+    cout  << "Hash (64-bit): 0x" << setw(16) << setfill('0') << hex << h << endl;
 
 }
-
-
-
-
